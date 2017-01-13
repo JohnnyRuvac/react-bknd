8de3107c5a32cb4e40a1e73ba2deb5c83171628d
Ondrej Rohon <ondrej.rohon@gmail.com>
2017-01-13 20:46:47 +0100

    Adding react.

--- client/app.js
+++ client/app.js
@@ -1,1 +1,9 @@
-console.log('meow');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Admin from './components/admin';
+
+
+ReactDOM.render(
+  <Admin />,
+  document.getElementById('app-container')
+);
