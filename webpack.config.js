be2850ec29f4993b28b926f8d07f35fa60b416cc
Ondrej Rohon <ondrej.rohon@gmail.com>
2017-02-04 23:29:23 +0100

    Separating entries and plugins based on env.

--- webpack.config.js
+++ webpack.config.js
@@ -5,23 +5,49 @@ const isProduction = process.argv.indexOf('-p') !== -1;
 const parsedEnvs = envHelper('./.env');
 
 
+const getEntries = () => {
+  if (!isProduction) {
+    return [
+      'react-hot-loader/patch',
+      // activate HMR for React
+
+      'webpack-dev-server/client?http://localhost:8080',
+      // bundle the client for webpack-dev-server
+      // and connect to the provided endpoint
+
+      'webpack/hot/only-dev-server',
+      // bundle the client for hot reloading
+      // only- means to only hot reload for successful updates
+      './index.js',
+    ];
+  } else {
+    return ['./index.js'];
+  }
+};
+
+let plugins = [
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': (isProduction) ? '"production"' : '""',
+      ...parsedEnvs
+    }
+  })
+];
+
+if (!isProduction) {
+  plugins = [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin(),
+    ...plugins,
+  ];
+}
+
+
 module.exports = {
   context: path.resolve(__dirname, 'client'),
   
-  devtool: (isProduction) ? '' : 'source-map',
-  entry: [
-    'react-hot-loader/patch',
-    // activate HMR for React
-
-    'webpack-dev-server/client?http://localhost:8080',
-    // bundle the client for webpack-dev-server
-    // and connect to the provided endpoint
-
-    'webpack/hot/only-dev-server',
-    // bundle the client for hot reloading
-    // only- means to only hot reload for successful updates
-    './index.js',
-  ],
+  devtool: (isProduction) ? false : 'source-map',
+  entry: getEntries(),
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist/assets'),
@@ -66,19 +92,6 @@ module.exports = {
     ],
   },
 
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': (isProduction) ? '"production"' : '""',
-        ...parsedEnvs
-      }
-    }),
-
-    new webpack.HotModuleReplacementPlugin(),
-    // enable HMR globally
-
-    new webpack.NamedModulesPlugin(),
-    // prints more readable module names in the browser console on HMR updates
-  ],
+  plugins: plugins,
 
 };
