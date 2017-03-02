12014a4eecd3520dd410881249c4aed068f37d18
Ondrej Rohon <ondrej.rohon@gmail.com>
2017-03-02 14:47:08 +0100

    Category item, adding new title, slug and texteditor components.

--- client/components/admin/Item/Item.js
+++ client/components/admin/Item/Item.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ContentType from '../ContentType/ContentType';
-import { Grid, Col, Row, Button, FormGroup, ControlLabel, FormControl, HelpBlock, Checkbox } from 'react-bootstrap';
+import { Grid, Col, Row, Button } from 'react-bootstrap';
 import TextEditor from '../TextEditor/TextEditor';
 import axios from 'axios';
 import { browserHistory } from 'react-router';
+import { TextInput, SlugInput } from '../Form';
 
 
 export default class Item extends ContentType {
@@ -28,51 +29,36 @@ export default class Item extends ContentType {
 
         <Row>
           <Col xs={12}>
-            <FormGroup
+            <TextInput 
               controlId="title"
-              validationState={this.getValidationState()}
-            >
-              <ControlLabel>Title</ControlLabel>
-              <FormControl
-                type="text"
-                placeholder="Title"
-                value={this.state.contentData.title}
-                onChange={this.handleChange.bind(this)}
-              />
-              <FormControl.Feedback />
-              <HelpBlock>Validation is based on string length.</HelpBlock>
-            </FormGroup>
+              label="Title"
+              placeholder="Title"
+              value={this.state.contentData.title}
+              onChange={this.updateContentDataState.bind(this)}
+            />
           </Col>
         </Row>
 
         <Row>
           <Col xs={12}>
-            <FormGroup
+            <SlugInput 
               controlId="slug"
-              validationState={this.getValidationState()}
-            >
-              <ControlLabel>{window.location.origin}/pages/</ControlLabel>
-              <FormControl
-                type="text"
-                placeholder="some-name"
-                value={this.state.contentData.slug}
-                onChange={this.handleSlugChange.bind(this)}
-              />
-              <FormControl.Feedback />
-              <HelpBlock>Validation is based on string length.</HelpBlock>
-            </FormGroup>
+              label="Slug"
+              placeholder="some-name"
+              value={this.state.contentData.slug}
+              titleValue={this.state.contentData.title}
+              onChange={this.updateContentDataState.bind(this)}
+            />
           </Col>
         </Row>
 
         <Row>
           <Col xs={12}>
-            <FormGroup controlId="content">
-              <ControlLabel>Text</ControlLabel>
-              <TextEditor
-                ref="te"
-                content={this.state.contentData.content}
-              />
-            </FormGroup>
+            <TextEditor
+              label="Content"
+              content={this.state.contentData.content}
+              ref="te"
+            />
           </Col>
         </Row>
 
